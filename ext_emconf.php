b2b583be306b40488d75bcc7fcfb9904670f00a9
René Alessandri <rene@alessandri-media.ch>
2022-03-02 14:43:20 +0100

    [TASK] Basic changes for v11.5 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '9.5.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0-10.9.999',
+			'typo3' => '9.5.0-11.9.999',
             'php' => '7.2.0-7.4.999',
         ],
 		'conflicts' => [],
